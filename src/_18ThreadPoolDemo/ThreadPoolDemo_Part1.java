b7af1e73079ae2fde7d2f99318bccf8b067775c3
chenxs <hewson.chen@qq.com>
2020-10-29 08:29:32 +0800

    新增单例模式DLC线程安全版的代码

--- src/_18ThreadPoolDemo/ThreadPoolDemo_Part1.java
+++ src/_18ThreadPoolDemo/ThreadPoolDemo_Part1.java
@@ -1,15 +1,18 @@
 package _18ThreadPoolDemo;
 
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ThreadPoolDemo_Part1 {
     public static void main(String[] args) {
-        //查看几个线程
+//        //查看几个线程
+//        ArrayList<String> strings = new ArrayList<>();
+//        LinkedList<String> strings1 = new LinkedList<>();
 //        System.out.println(Runtime.getRuntime().availableProcessors());
-        ExecutorService threadpool = Executors.newFixedThreadPool(5);//一池5个线程
+        ExecutorService threadpool = Executors.newFixedThreadPool(5);//一池固定5个线程
         ExecutorService threadpool2 = Executors.newSingleThreadExecutor();
-        ExecutorService threadpool3 = Executors.newCachedThreadPool();
+        ExecutorService threadpool3 = Executors.newCachedThreadPool();//空闲线程默认60秒后过期
 
 //        模拟10个用户办理业务，每个用户就算一个来自外部的请求线程
         try {
--- src/_1Volatile/VolatileDemo.java
+++ src/_1Volatile/VolatileDemo.java
@@ -1,4 +1,16 @@
 package _1Volatile;
 
+//单例模式，DCL多线程安全版
 public class VolatileDemo {
+    private volatile VolatileDemo volatileDemo = null;
+    public VolatileDemo getInstance(){
+        if (volatileDemo ==null){
+            synchronized (VolatileDemo.class){
+                if (volatileDemo == null){
+                    volatileDemo = new VolatileDemo();
+                }
+            }
+        }
+        return volatileDemo;
+    }
 }
